43c27cfdef22536f14da35e8332f47aa9eca3560
Jonas Spinner <jonasspinner@gmx.de>
2025-05-27 16:22:50 +0200

    Add cross attention and conditional LGATr

--- lgatr/__init__.py
+++ lgatr/__init__.py
@@ -3,14 +3,17 @@ from .interface.scalar import embed_scalar, extract_scalar
 from .interface.vector import embed_vector, extract_vector
 from .interface.pseudoscalar import embed_pseudoscalar, extract_pseudoscalar
 from .interface.axialvector import embed_axialvector, extract_axialvector
-from .layers.attention.config import SelfAttentionConfig
+from .layers.attention.config import SelfAttentionConfig, CrossAttentionConfig
 from .layers.mlp.config import MLPConfig
 from .nets.lgatr import LGATr
+from .nets.conditional_lgatr import ConditionalLGATr
 from .primitives.config import gatr_config
 
 __all__ = [
     "LGATr",
+    "ConditionalLGATr",
     "SelfAttentionConfig",
+    "CrossAttentionConfig",
     "MLPConfig",
     "embed_scalar",
     "extract_scalar",
