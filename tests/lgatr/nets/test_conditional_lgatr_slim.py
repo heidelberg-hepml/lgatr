24bbf95fec811e311c73ad097a3e6ec5bb080a2f
Jonas Spinner <jonasspinner@gmx.de>
2025-12-23 23:11:15 +0100

    Add unit test for compiled ConditionalLGATrSlim

--- tests/lgatr/nets/test_conditional_lgatr_slim.py
+++ tests/lgatr/nets/test_conditional_lgatr_slim.py
@@ -167,3 +167,68 @@ def test_ConditionalLGATrSlim_equivariance(
         fn_kwargs=dict(scalars=s, scalars_condition=cond_s),
         **TOLERANCES,
     )
+
+
+@pytest.mark.parametrize("batch_dims", BATCH_DIMS)
+@pytest.mark.parametrize("N,Ncond", [(3, 7)])
+@pytest.mark.parametrize(
+    "in_v_channels,in_s_channels,out_v_channels,out_s_channels,cond_v_channels,cond_s_channels",
+    [(4, 3, 9, 2, 5, 6)],
+)
+@pytest.mark.parametrize(
+    "hidden_v_channels,hidden_s_channels,num_heads,num_blocks", [(16, 8, 4, 1)]
+)
+def test_ConditionalLGATrSlim_equivariance_compiled(
+    batch_dims,
+    N,
+    Ncond,
+    in_v_channels,
+    in_s_channels,
+    out_v_channels,
+    out_s_channels,
+    cond_v_channels,
+    cond_s_channels,
+    hidden_v_channels,
+    hidden_s_channels,
+    num_heads,
+    num_blocks,
+    compile=True,
+):
+    layer = ConditionalLGATrSlim(
+        in_v_channels=in_v_channels,
+        condition_v_channels=cond_v_channels,
+        out_v_channels=out_v_channels,
+        hidden_v_channels=hidden_v_channels,
+        in_s_channels=in_s_channels,
+        condition_s_channels=cond_s_channels,
+        out_s_channels=out_s_channels,
+        hidden_s_channels=hidden_s_channels,
+        num_blocks=num_blocks,
+        num_heads=num_heads,
+        compile=compile,
+    )
+    layer.eval()
+
+    s = torch.randn(*batch_dims, N, in_s_channels)
+    cond_s = torch.randn(*batch_dims, Ncond, cond_s_channels)
+    v = torch.randn(*batch_dims, N, in_v_channels, 4)
+    cond_v = torch.randn(*batch_dims, Ncond, cond_v_channels, 4)
+
+    out_v, out_s = layer(
+        vectors=v,
+        vectors_condition=cond_v,
+        scalars=s,
+        scalars_condition=cond_s,
+    )
+    assert out_v.shape == v.shape[:-2] + (out_v_channels, 4)
+    assert out_s.shape == s.shape[:-1] + (out_s_channels,)
+
+    # equivariance
+    batch_dims = [batch_dims + [N, in_v_channels], batch_dims + [Ncond, cond_v_channels]]
+    check_equivariance(
+        layer,
+        batch_dims=batch_dims,
+        num_args=2,
+        fn_kwargs=dict(scalars=s, scalars_condition=cond_s),
+        **TOLERANCES,
+    )
