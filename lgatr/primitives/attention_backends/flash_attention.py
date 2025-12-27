64f9bc43baf7aa41a59fd10b9df9f295a3703277
Jonas Spinner <jonasspinner@gmx.de>
2025-12-27 12:03:39 +0100

    Make flash-attention work for now

--- lgatr/primitives/attention_backends/flash_attention.py
+++ lgatr/primitives/attention_backends/flash_attention.py
@@ -1,5 +1,7 @@
 """original flash-attention backend."""
 
+import torch
+
 try:
     from flash_attn import flash_attn_varlen_func
 except ModuleNotFoundError as err:
@@ -7,6 +9,17 @@ except ModuleNotFoundError as err:
         "flash-attn is not installed. Run 'pip install lgatr[flash-attention]'."
     ) from err
 
+
 # There is no fancy docs website, so one has to check the source code for the interface:
 # https://github.com/Dao-AILab/flash-attention/blob/main/flash_attn/flash_attn_interface.py
-attention = flash_attn_varlen_func
+def attention(q, k, v, dtype=torch.bfloat16, **kwargs):
+    in_dtype = q.dtype
+
+    def prepare(x):
+        assert x.shape[0] == 1
+        return x.to(dtype).squeeze(0).transpose(0, 1).contiguous()
+
+    q, k, v = prepare(q), prepare(k), prepare(v)
+    out = flash_attn_varlen_func(q, k, v, **kwargs)
+    out = out.transpose(0, 1).unsqueeze(0).contiguous()
+    return out.to(in_dtype)
