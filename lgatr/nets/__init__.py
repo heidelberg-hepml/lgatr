bbb08a759dee242dd997e6f8c80459baa8733c82
Antoine Petitjean <antoine.petitjean1@ens-lyon.fr>
2026-02-05 18:43:43 +0100

    sort imports

--- lgatr/nets/__init__.py
+++ lgatr/nets/__init__.py
@@ -1,4 +1,4 @@
 from .conditional_lgatr import ConditionalLGATr
-from .lgatr import LGATr
 from .conditional_lgatr_slim import ConditionalLGATrSlim
+from .lgatr import LGATr
 from .lgatr_slim import LGATrSlim
