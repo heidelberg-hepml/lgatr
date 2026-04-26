54da5d2426ce32b7121b0b39db93fd3900558374
Jonas Spinner <jonasspinner@gmx.de>
2026-04-26 22:53:44 +0200

    Catch up with ConditionalLGATrSlim

--- lgatr/__init__.py
+++ lgatr/__init__.py
@@ -8,6 +8,7 @@ from .interface.vector import embed_vector, extract_vector
 from .layers.attention.config import CrossAttentionConfig, SelfAttentionConfig
 from .layers.mlp.config import MLPConfig
 from .nets.conditional_lgatr import ConditionalLGATr
+from .nets.conditional_lgatr_slim import ConditionalLGATrSlim
 from .nets.lgatr import LGATr
 from .nets.lgatr_slim import LGATrSlim
 from .primitives.config import gatr_config
