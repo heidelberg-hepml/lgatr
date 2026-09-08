7bc789a9181e8cb7f97269b334cc449c22ca2f37
t0mnt <199419433+t0mnt@users.noreply.github.com>
2026-09-08 00:53:14 -0400

    Add inner_product_factors to imports in __init__.py

--- lgatr/primitives/__init__.py
+++ lgatr/primitives/__init__.py
@@ -4,6 +4,6 @@ from .attention import sdp_attention
 from .bilinear import geometric_product
 from .config import PrimitivesConfig
 from .dropout import grade_dropout
-from .invariants import abs_squared_norm, inner_product
+from .invariants import abs_squared_norm, inner_product, inner_product_factors
 from .linear import equi_linear, grade_involute, grade_project, reverse
 from .normalization import equi_layer_norm
